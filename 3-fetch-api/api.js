5ee27193c82cbd20a1eb5255ca4371b4694d0a2f
Tri Phan <phancaotri2212@gmail.com>
2023-08-15 14:27:29 +0700

    add description

--- 3-fetch-api/api.js
+++ 3-fetch-api/api.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://64d9d6d1e947d30a260a5be1.mockapi.io/'
 
 async function fetchTodoList() {
-  const resp = await fetch(baseUrl + 'v1/todos')
+  const resp = await fetch(baseUrl + 'v1/todos') // get request
   const json = await resp.json();
 
   return json; // [{},{}]
@@ -15,12 +15,14 @@ async function addTodo(description) {
     createdAt: new Date()
   }
 
+  //pop request
   const resp = await fetch(baseUrl + 'v1/todos', {
     method: 'POST',
     body: JSON.stringify(payload)
   })
 }
 
+// delete request
 async function deleteTodo(id) {
   const resp = await fetch(baseUrl + 'v1/todos/' + id, {
     method: 'DELETE'
